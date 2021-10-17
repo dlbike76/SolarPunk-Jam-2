5893fdc1fc349b5eaa42393cd5218e73e51b86e2
Robert-Bowie <collazo.valentino@gmail.com>
2021-10-16 22:56:03 -0300

    added camera paning

--- Source/Engine/Camera.gd
+++ Source/Engine/Camera.gd
@@ -4,17 +4,23 @@ onready var player := get_parent().get_node("Player")
 
 var triggered := false
 
+
+
 func _process(delta: float) -> void:
-	var broken_machines := get_tree().get_nodes_in_group("broken_machines")
+	var broken_machines := get_tree().get_nodes_in_group("Broken_Machines")
 	if Input.is_action_pressed("search"): 
+		var triggered = true
 		for node in broken_machines:
 			pan_to_object(node)
-			yield(get_tree().create_timer(2.0), "timeout")
-	else:
+			yield(get_tree().create_timer(1.0), "timeout")
+		triggered = false
+	if triggered == false:
+	#else:
+		self.offset = Vector2 (0,0)
 		global_position.y = move_toward(global_position.y, player.position.y, 2.5)
 		global_position.x = move_toward(global_position.x, player.position.x, 2.5)
 
 
 func pan_to_object(node):
-	global_position.y = move_toward(global_position.y, node.position.y, 5)
-	global_position.x = move_toward(global_position.x, node.position.x, 5)
+	self.offset = Vector2(node.global_position.x-self.global_position.x,node.global_position.y-self.global_position.y)
+
--- Source/Non-Solids/Interaction_Objects/Vine_Trigger.gd
+++ Source/Non-Solids/Interaction_Objects/Vine_Trigger.gd
@@ -4,14 +4,21 @@ onready var animated_sprite := $AnimatedSprite
 onready var ladder := $Ladder
 onready var sfx_grow := $Grow
 
+onready var the_player : KinematicBody2D =  get_parent().get_parent().get_parent().get_node("Player")
+
+onready var player_energy = the_player.mental_energy
+
+var used = false
 var charge = 0.0
 var played := false
+var timer = 0.0
 
 export var vine_height := 100
 export var charge_rate := 30.0
 
+
 func _process(delta: float) -> void:
-	if charge > 90:
+	if (charge > 90 ):
 		vine_ladder_grow()
 		animated_sprite.play("grow")
 	elif (Game.check_player_collision(self,Vector2(0,0)) and Input.is_action_pressed("action")): 
@@ -19,6 +26,13 @@ func _process(delta: float) -> void:
 		animated_sprite.play("charging")
 	else: 
 		animated_sprite.play("idle")
+	
+	if used == true:
+		timer += delta
+	
+	if timer >= 120:
+		used = false
+		charge = 0.0
 
 func vine_ladder_grow():
 	ladder.hitbox.height = vine_height
